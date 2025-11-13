dc1f33b52d5a0ea2ebc52d3f47d93064896ec4ce
smailnajim <smailnajim2003@gmail.com>
2025-11-13 19:54:22 +0100

    problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
+// Import everything needed to use the `useQuery` hook
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
 
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
 
-function App() {
+function DisplayLocations() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
 
-  return (
-    <>
-      
-    </>
-  )
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.locations.map(({ id, name, description, photo }) => (
+    <div key={id}>
+      <h3>{name}</h3>
+      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+      <br />
+      <b>About this location:</b>
+      <p>{description}</p>
+      <br />
+    </div>
+  ));
 }
 
-export default App
+
+
+export default function App() {
+  return (
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <br />
+      <DisplayLocations />
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const client = new ApolloClient({
+  uri: "https://flyby-router-demo.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
+
+// Supported in React 18+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
-)
+  </ApolloProvider>
+);
